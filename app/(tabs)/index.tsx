899570e69d56c0100f30517de70abdef92b96cf0
japjyot.suri <japjyot.suri@geekyants.com>
2025-12-04 16:50:56 +0530

    feat: Added code for animation and scaling fix for background image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -11,7 +11,7 @@ const brochureData = {
     height: 737,
   },
   stage:
-    '{"attrs":{"width":552,"height":737},"className":"Stage","children":[{"attrs":{},"className":"Layer","children":[{"attrs":{"id":"bg","width":552,"height":737,"fill":"#e7caa5"},"className":"Rect"},{"attrs":{"x":-1,"y":-1,"draggable":true,"id":"7baeab86-940b-4cb7-a3e5-877b02eb9908","width":552.9999999999998,"height":737.9999999999998,"opacity":0.66,"cropWidth":1080,"cropHeight":1620},"className":"Image"},{"attrs":{"x":336.9002240356156,"y":666.5525495306292,"rotation":-0.7779925231303255,"scaleX":1.0650212096146463,"scaleY":1.0650212096146463,"draggable":true,"id":"7cac8df7-02ea-463f-b14c-101d88ea4e93","text":"Join us here","fontSize":26,"fill":"#050505","fontWeight":"bold","skewX":1.1272810827459567e-16,"opacity":0.9666174398337986},"className":"Text"},{"attrs":{"x":361,"draggable":true,"id":"32222791-5d6e-44f5-85da-6482a7120866","width":200,"height":200,"fill":"#E0E0E0","opacity":0.3,"stroke":"#999","strokeWidth":1},"className":"Rect"},{"attrs":{"x":129.00000000000003,"y":191,"draggable":true,"id":"13286dfc-9e5f-46e0-bda1-2262d619b077","width":227.00000000000006,"height":317.2859084730396,"cropWidth":1080,"cropHeight":1532},"className":"Image"},{"attrs":{"x":178.99999999999994,"y":219,"draggable":true,"id":"956b0665-0dcd-4f2c-a496-cd001036e0da","width":124.00000000000007,"height":32.14285714285705},"className":"Group","children":[{"attrs":{"width":124.00000000000007,"height":32.14285714285705,"fill":"#070708","cornerRadius":34,"shadowBlur":6,"shadowColor":"black"},"className":"Rect"},{"attrs":{"text":"View more","fontSize":22,"fill":"#ffffff","align":"center","verticalAlign":"middle","width":124.00000000000007,"height":32.14285714285705,"y":5.071428571428527},"className":"Text"}]},{"attrs":{"x":400.9999999999999,"y":157.9999999999998,"draggable":true,"id":"4e44ac5b-08d7-4008-9952-922739457a1a","innerRadius":30,"outerRadius":70,"fill":"#ffffff","stroke":"#a6c983","strokeWidth":20},"className":"Star"},{"attrs":{"x":6,"y":9,"draggable":true,"id":"1","text":"Italy Diaries","fontSize":58,"fill":"#e6aa41","fontFamily":"Georgia","fontWeight":"bold","textDecoration":"none"},"className":"Text"},{"attrs":{"x":0.746268656716418,"y":577.7180616740089,"draggable":true,"id":"04757c29-b9ec-4a17-a7a2-9209fd711ec2","width":298.5074626865672,"height":161.5638766519821,"fill":"#000000","opacity":0.3,"stroke":"#666","strokeWidth":1},"className":"Rect"},{"attrs":{"keepRatio":false,"anchorSize":8,"x":132.00000000000003,"y":193},"className":"Transformer"}]}]}',
+    '{"attrs":{"width":552,"height":737},"className":"Stage","children":[{"attrs":{},"className":"Layer","children":[{"attrs":{"id":"bg","width":552,"height":737,"fill":"#e7caa5"},"className":"Rect"},{"attrs":{"x":-1,"y":-1,"scaleX":1.1962665928350547,"scaleY":1.1962665928350547,"draggable":true,"id":"7baeab86-940b-4cb7-a3e5-877b02eb9908","width":552.9999999999998,"height":737.9999999999998,"opacity":0.66,"cropWidth":1080,"cropHeight":1620},"className":"Image"},{"attrs":{"x":336.9002240356156,"y":666.5525495306292,"scaleX":1.1200337934741231,"scaleY":1.1200337934741231,"draggable":true,"id":"7cac8df7-02ea-463f-b14c-101d88ea4e93","text":"Join us here","fontSize":26,"fill":"#050505","fontWeight":"bold"},"className":"Text"},{"attrs":{"x":361,"draggable":true,"id":"32222791-5d6e-44f5-85da-6482a7120866","width":200,"height":200,"fill":"#E0E0E0","opacity":0.3,"stroke":"#999","strokeWidth":1},"className":"Rect"},{"attrs":{"x":129.00000000000003,"y":191,"draggable":true,"id":"13286dfc-9e5f-46e0-bda1-2262d619b077","width":227.00000000000006,"height":317.2859084730396,"cropWidth":1080,"cropHeight":1532},"className":"Image"},{"attrs":{"x":178.99999999999994,"y":219,"draggable":true,"id":"956b0665-0dcd-4f2c-a496-cd001036e0da","width":124.00000000000007,"height":32.14285714285705},"className":"Group","children":[{"attrs":{"width":124.00000000000007,"height":32.14285714285705,"fill":"#070708","cornerRadius":34,"shadowBlur":6,"shadowColor":"black"},"className":"Rect"},{"attrs":{"text":"View more","fontSize":22,"fill":"#ffffff","align":"center","verticalAlign":"middle","width":124.00000000000007,"height":32.14285714285705,"y":5.071428571428527},"className":"Text"}]},{"attrs":{"x":352.38884747905024,"y":139.24052642021255,"rotation":213.64199999973178,"scaleX":1.13927277425239,"scaleY":1.13927277425239,"draggable":true,"id":"4e44ac5b-08d7-4008-9952-922739457a1a","innerRadius":30,"outerRadius":70,"fill":"#ffffff","stroke":"#a6c983","strokeWidth":20,"opacity":0.9752481883155353},"className":"Star"},{"attrs":{"x":6,"y":9,"draggable":true,"id":"1","text":"Italy Diaries","fontSize":58,"fill":"#e6aa41","fontFamily":"Georgia","fontWeight":"bold","textDecoration":"none"},"className":"Text"},{"attrs":{"x":0.746268656716418,"y":577.7180616740089,"draggable":true,"id":"04757c29-b9ec-4a17-a7a2-9209fd711ec2","width":298.5074626865672,"height":161.5638766519821,"fill":"#000000","opacity":0.3,"stroke":"#666","strokeWidth":1},"className":"Rect"},{"attrs":{"x":49.5,"y":110.49999999999983,"rotation":345.6719999997318,"scaleX":1.1200337934741231,"scaleY":1.1200337934741231,"draggable":true,"id":"da5dc991-a8e2-4097-a96c-4678b6eb6d86","radius":42.6470588235294,"fill":"#ffffff","stroke":"#333","strokeWidth":4},"className":"Circle"},{"attrs":{"x":63.5,"y":256.5966086828001,"rotation":341.22600000053643,"scaleX":1.187507450860299,"scaleY":1.187507450860299,"draggable":true,"id":"b92fbee8-c56f-4dda-8573-5ab9a4648ff0","radius":42.6470588235294,"fill":"#ffffff","stroke":"#333","strokeWidth":4},"className":"Circle"},{"attrs":{"x":67.5,"y":405.38026543445176,"draggable":true,"id":"e44ad22a-f6e7-467b-bb8c-2901c1ea9459","radius":42.6470588235294,"fill":"#ffffff","stroke":"#333","strokeWidth":4,"opacity":0.7201182771594306},"className":"Circle"},{"attrs":{"x":171.5,"y":144.9999999999999,"rotation":36.88200000107288,"draggable":true,"id":"ceda915a-e157-44ca-892b-a529d61504e3","innerRadius":30,"outerRadius":70,"fill":"#ffffff","stroke":"#333","strokeWidth":4},"className":"Star"},{"attrs":{"x":431.0903317264122,"y":338,"draggable":true,"id":"91441566-30af-441a-a355-372621bbbcb8","radiusX":80,"radiusY":40,"fill":"#ffffff","stroke":"#333","strokeWidth":4},"className":"Ellipse"},{"attrs":{"keepRatio":false,"anchorSize":8,"x":336.5810467981125},"className":"Transformer"}]}]}',
   items: [
     {
       id: "b60ac386-fed6-473c-ba20-77f5a8fa2739",
@@ -21,7 +21,7 @@ const brochureData = {
       y: 247,
       width: 200,
       height: 200,
-      src: "https://images.unsplash.com/photo-1763906667589-ae7b7d60a371?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w4MjY5MjJ8MHwxfGFsbHw1Mnx8fHx8fDJ8fDE3NjQ2NjAyMDl8&ixlib=rb-4.1.0&q=80&w=1080",
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRInKFKLo7HSnAQ5DGzi7s0OwOSoQ7qxFHyrA&s",
       img: null,
       opacity: 1,
       visible: true,
@@ -76,13 +76,11 @@ const brochureData = {
       y: 0,
       width: 200,
       height: 200,
-      src: "images/image_1.webp",
+      src: "https://raw.githubusercontent.com/JapjyotSuri/GifUrl/main/just-do-it-eric-cartman-ezgif.com-webp-to-gif-converter.gif",
       img: null,
       opacity: 1,
       visible: true,
       locked: false,
-      originalSrc:
-        "https://cdn.pixabay.com/animation/2025/11/03/21/48/21-48-26-427_512.gif",
     },
     {
       id: "13286dfc-9e5f-46e0-bda1-2262d619b077",
@@ -168,7 +166,9 @@ const brochureData = {
       y: 577.7180616740089,
       width: 298.5074626865672,
       height: 161.5638766519821,
-      src: "videos/video_1.mp4",
+      originalSrc:
+        "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+      src: "hello.mp4",
       video: null,
       autoplay: true,
       loop: true,
@@ -178,8 +178,116 @@ const brochureData = {
       rotation: 0,
       scaleX: 1,
       scaleY: 1,
-      originalSrc:
-        "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    },
+    {
+      id: "da5dc991-a8e2-4097-a96c-4678b6eb6d86",
+      type: "circle",
+      name: "Circle",
+      x: 49.5,
+      y: 110.49999999999983,
+      radius: 42.6470588235294,
+      fill: "#ffffff",
+      stroke: "#333",
+      strokeWidth: 4,
+      visible: true,
+      locked: false,
+      rotation: 0,
+      scaleX: 1,
+      scaleY: 1,
+      animation: {
+        type: "pulse",
+        duration: 2000,
+        infinite: true,
+        direction: "alternate",
+      },
+    },
+    {
+      id: "b92fbee8-c56f-4dda-8573-5ab9a4648ff0",
+      type: "circle",
+      name: "Circle Copy",
+      x: 63.5,
+      y: 268.5999880302124,
+      radius: 42.6470588235294,
+      fill: "#ffffff",
+      stroke: "#333",
+      strokeWidth: 4,
+      visible: true,
+      locked: false,
+      rotation: 0,
+      scaleX: 1,
+      scaleY: 1,
+      animation: {
+        type: "bounce",
+        duration: 2000,
+        infinite: true,
+        direction: "alternate",
+      },
+    },
+    {
+      id: "e44ad22a-f6e7-467b-bb8c-2901c1ea9459",
+      type: "circle",
+      name: "Circle Copy Copy",
+      x: 67.5,
+      y: 405.38026543445176,
+      radius: 42.6470588235294,
+      fill: "#ffffff",
+      stroke: "#333",
+      strokeWidth: 4,
+      visible: true,
+      locked: false,
+      rotation: 0,
+      scaleX: 1,
+      scaleY: 1,
+      animation: {
+        type: "fade",
+        duration: 2000,
+        infinite: true,
+        direction: "alternate",
+      },
+    },
+    {
+      id: "ceda915a-e157-44ca-892b-a529d61504e3",
+      type: "star",
+      name: "Star",
+      x: 171.5,
+      y: 144.9999999999999,
+      numPoints: 5,
+      innerRadius: 30,
+      outerRadius: 70,
+      fill: "#ffffff",
+      stroke: "#333",
+      strokeWidth: 4,
+      visible: true,
+      locked: false,
+      rotation: 0,
+      scaleX: 1,
+      scaleY: 1,
+      animation: {
+        type: "rotate",
+        duration: 2000,
+        infinite: true,
+        direction: "alternate",
+      },
+    },
+    {
+      id: "91441566-30af-441a-a355-372621bbbcb8",
+      type: "ellipse",
+      name: "Ellipse",
+      x: 401.08188335788145,
+      y: 338,
+      radiusX: 80,
+      radiusY: 40,
+      fill: "#ffffff",
+      stroke: "#333",
+      strokeWidth: 4,
+      visible: true,
+      locked: false,
+      animation: {
+        type: "slide",
+        duration: 2000,
+        infinite: true,
+        direction: "alternate",
+      },
     },
   ],
   videos: [
@@ -191,7 +299,9 @@ const brochureData = {
       y: 577.7180616740089,
       width: 298.5074626865672,
       height: 161.5638766519821,
-      src: "videos/video_1.mp4",
+      src: "hello.mp4",
+      originalSrc:
+        "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
       video: null,
       autoplay: true,
       loop: true,
@@ -201,8 +311,6 @@ const brochureData = {
       rotation: 0,
       scaleX: 1,
       scaleY: 1,
-      originalSrc:
-        "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
     },
   ],
   images: [
@@ -214,16 +322,14 @@ const brochureData = {
       y: 0,
       width: 200,
       height: 200,
-      src: "images/image_1.webp",
+      src: "https://raw.githubusercontent.com/JapjyotSuri/GifUrl/main/just-do-it-eric-cartman-ezgif.com-webp-to-gif-converter.gif",
       img: null,
       opacity: 1,
       visible: true,
       locked: false,
-      originalSrc:
-        "blob:http://localhost:3000/40cc71c1-08f6-490a-8aa5-e644b62ed4a0",
     },
   ],
-  exportDate: "2025-12-02T07:51:16.166Z",
+  exportDate: "2025-12-03T12:57:28.466Z",
   videoCount: 1,
   imageCount: 1,
 };
@@ -277,24 +383,21 @@ html, body, #konva-container {
         };
     })();
 
-
   const jsonData = ${JSON.stringify(data)};
   const stageJSON = JSON.parse(jsonData.stage);
   const originalWidth = jsonData.canvas.width;
   const originalHeight = jsonData.canvas.height;
- 
+
 
   const container = document.getElementById("konva-container");
   const displayedWidth = container.offsetWidth;
   const displayedHeight = container.offsetHeight;
-  console.log('[DISPLAYED] Width:', displayedWidth, 'Height:', displayedHeight);
   const scaleX = displayedWidth / originalWidth;
   const scaleY = displayedHeight / originalHeight;
-  console.log('[SCALE] X:', scaleX, 'Y:', scaleY);
   const scale = Math.min(scaleX, scaleY);
 
   const stage = Konva.Node.create(stageJSON, "konva-container");
-  
+
   stage.scale({ x: scale, y: scale });
   stage.x((displayedWidth - originalWidth * scale)/2);
   stage.y((displayedHeight - originalHeight * scale)/2);
@@ -302,27 +405,96 @@ html, body, #konva-container {
   stage.height(displayedHeight);
   stage.draw();
 
- 
+  function applyAnimationsToNode(node, item) {
+    if (item.animation === undefined) return;
+    const { duration, infinite, direction } = item.animation;
+    
+    console.log('[ANIMATION] animation:', item.animation);
+    switch (item.animation.type) {
+      case "pulse":
+        const anim = new Konva.Animation(function(frame) {
+        const progress = (frame.time % duration) / duration;
+        // const scale = Math.sin( Math.PI * 2 * progress);
+        const scaleAnim = (Math.sin(frame.time * 1.75 * Math.PI / duration) * scale * 0.3) + 1;
+        // node.scale({x: scale, y: scale});
+        node.scaleX(scaleAnim);
+        node.scaleY(scaleAnim);
+      }, node.getLayer());
+      anim.start();
+      break;
+      case "fade":
+        const fadeAnim = new Konva.Animation(function(frame) {
+        const progress = (frame.time % duration) / duration;
+        const opacAnim = (Math.sin(frame.time * 2.5 * Math.PI / duration)) + 1.4;
+        node.opacity(opacAnim);
+      }, node.getLayer());
+      fadeAnim.start();
+      break;
+      case "bounce":
+        const originalY = node.y();
+        const bounceDistance = 20;
+         const tween = new Konva.Tween({
+           node: node,
+            duration: duration/2000,
+             y: originalY + bounceDistance,
+             easing: Konva.Easings.Bounce,
+             yoyo: true,
+         });
+         tween.play();
+        break;
+      case "rotate":
+        const rotateTween = new Konva.Tween({
+          node: node,
+          duration: duration/1000,
+          rotation: 360,
+        });
+       onFinish: () => {
+        // Reset rotation so the angle doesnt overflow
+        node.rotation(0);
+        startRotation(); // restart again for infinite loop
+      },
+        rotateTween.play();
+        break;
+      case "slide":
 
+        break;
+    };
+}
+     
   jsonData.items.forEach((item) => {
    const node = stage.findOne("#" + item.id);
     if (!node) return;
     //making items not draggable
-    node.draggable(false);
+    node.draggable(false); 
     // TEXT and SHAPES are already rendered via JSON
     if (["text", "rect", "circle", "line", "path"].includes(item.type)) {
-      return; 
-}
+      //do nothing
+    }
+
     const layer = node.getLayer();
     // IMAGE HANDLING
     if (item.type === "image") {
-    //Creating a browser Image element
+    const imgLayer = new Konva.Layer();
+    stage.add(imgLayer);
       const img = new Image();
       img.crossOrigin = "anonymous";
       img.src = item.src;
 
       img.onload = () => {
-        node.image(img);
+      const konvaImage = new Konva.Image({
+      x: item.x,
+      y: item.y,
+      image: img,
+      opacity: item.opacity ?? 1,
+      rotation: item.rotation ?? 0,
+      width: item.width,
+      height: item.height,
+      draggable: false,
+      scaleX: item.scaleX ?? 1,
+      scaleY: item.scaleY ?? 1,
+  });
+        layer.add(konvaImage);
+        konvaImage.zIndex(node.zIndex());
         //scheduling a redraw here to ensure the image is displayed
         stage.batchDraw();
       };
@@ -333,7 +505,6 @@ html, body, #konva-container {
     const layer = node.getLayer();
     const videoLayer = new Konva.Layer();
     stage.add(videoLayer);
-    console.log('[VIDEO] Layer found:', videoLayer ? 'yes' : 'no');
     const videoElement = document.createElement('video');
     videoElement.src = item.originalSrc;
     videoElement.muted = true;
@@ -354,11 +525,8 @@ html, body, #konva-container {
     }, videoLayer);
 
   videoElement.addEventListener('loadstart', function () {
-
-        sendMessage('loadstart');
       });
   videoElement.addEventListener('loadedmetadata', function () {
-
     image.width(item.width);
     image.height(item.height);
 });
@@ -369,7 +537,6 @@ html, body, #konva-container {
             anim.start();
           }).catch(error => {
             console.error('[VIDEO] Autoplay failed:', error);
-            text.text('Click PLAY to start');
             stage.batchDraw();
           });
         }
@@ -408,13 +575,10 @@ const image = new Konva.Image({
 gifLayer.add(image);
 
   }
-if (item.type === "link") {
+if (item.type === "link" || item.type === "button") {
   const linkNode = stage.findOne("#" + item.id);
-  console.log('[LINK] Link Node:', linkNode);
   if (linkNode) {
-    console.log('[LINK] Found:', linkNode ? 'yes' : 'no');
     linkNode.on('touchend', () => {
-  console.log('[LINK] Clicked:', item.url);
   window.ReactNativeWebView.postMessage(
     JSON.stringify({
       type: "link_click",
@@ -424,6 +588,8 @@ if (item.type === "link") {
 });
   }
 }
+
+  // applyAnimationsToNode(node, item);
  });
 </script>
 </body>
@@ -440,6 +606,7 @@ export default function TabOneScreen() {
           javaScriptEnabled={true}
           onMessage={(event) => {
             try {
+              console.log("[MESSAGE] Message:", event.nativeEvent.data);
               const data = JSON.parse(event.nativeEvent.data);
 
               if (data.type === "link_click") {
@@ -469,8 +636,8 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   webviewContainer: {
-    width: "100%",
-    height: "100%",
+    width: "70%",
+    height: "70%",
   },
   title: {
     fontSize: 20,
